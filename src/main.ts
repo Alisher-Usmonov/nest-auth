ad36d5085688e84a7cce0f97d8638f5fe1d7052d
Alisher-Usmonov <alisherufullstack@gmail.com>
2022-06-16 21:39:39 +0500

    procfile & commands

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,12 @@ import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
   app.use(cookieParser());
-
+  app.enableCors();
   app.setBaseViewsDir(join(__dirname, "..", "views"));
   app.setViewEngine("ejs");
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
